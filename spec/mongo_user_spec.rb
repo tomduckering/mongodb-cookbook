d47474b6664c5dbd3efa5300d11f00ca3071ea96
Eduardo Aquiles Radanovitsck <eradanov@thoughtworks.com>
2014-11-13 17:06:55 +0000

    Tom/Eduardo/Brady - add test for joebloggs user.

--- spec/mongo_user_spec.rb
+++ spec/mongo_user_spec.rb
@@ -3,7 +3,11 @@ require_relative 'chefspec_helper'
 describe 'mongodb::auth_with_users_instance' do
   let(:chef_run) { ChefSpec::SoloRunner.new(step_into: ['user']).converge(described_recipe) }
 
-  it 'should create a user' do
+  it 'should create an admin user' do
     expect(chef_run).to create_mongodb_user('admin')
+  end
+
+  it 'should create a normal user' do
+    expect(chef_run).to create_mongodb_user('joebloggs')
   end
 end
