0a0edb9191c5f10a5dddc1e1113a5db65098f757
Thomas Duckering <tduckeri@thoughtworks.com>
2014-11-14 13:00:20 +0000

    Tom - admin details allowed. Need to add functionality to do replicaset when auth is on.

--- resources/replicaset.rb
+++ resources/replicaset.rb
@@ -5,3 +5,5 @@ attribute :name, :name_attribute => true, :kind_of => String, :required => true
 attribute :config_document, :kind_of => Hash, :required => true
 attribute :host, :kind_of => String, :default => 'localhost'
 attribute :port, :kind_of => Integer, :default => 27017
+attribute :admin_user, :kind_of => String, :default => nil
+attribute :admin_password, :kind_of => String, :default => nil
